7671c9291fdc4c57bd3928b157a5dc394215e5e2
Riteej <72737661+Riteej@users.noreply.github.com>
2020-12-28 15:47:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var ball;
+var database,position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+
+    ball = createSprite(250,250,10,10);// x=250,y=250
+    ball.shapeColor = "red";
+
+    var ballpos=database.ref('ball/position');
+    ballpos.on("value",readposition,showError);
+}
+
+function draw(){
+    background("white");
+    if( position!==undefined)
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+database.ref('ball/position').set({
+    'x':position.x+x,
+    'y':position.y+y
+})
+}
+
+function readposition(data)
+{
+    position=data.val(); //x=50,y=50 inside database
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showError()
+{
+  console.log("error in reading database")  
+}
